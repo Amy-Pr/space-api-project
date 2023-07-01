56572ffb4b586e5a0ec9362d7b4e9871e85dfd02
amy_pr <ampreci@gmail.com>
2023-07-01 11:41:09 -0700

    fix syntax of promise chain

--- js/promises-breakfast.js
+++ js/promises-breakfast.js
@@ -1,7 +1,14 @@
+const order = false;
+
 const breakfastPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        //resolve('Your order is ready. Come and get it!');//What is passed to the resolve method is available to be used in the "then" method.
-        reject('Oh no! There was a problem with your order.')
+        if(order) {
+            resolve('Your order is ready. Come and get it!'); //What is passed to the resolve method is available to be used in the "then" method.
+
+        } else {
+            reject(Error('Your order cannot be made.')); //'Error' not needed but helps with debugging.
+        }
+        
     }, 3000)
 
 });
@@ -10,4 +17,6 @@ console.log(breakfastPromise)
 //To get the promise value out of the promise object (consume it), use the then() method. 
 //It can be called to handle fulfilled and rejected promises.
 //The catch method only handles rejections, or errors.
-breakfastPromise.then( value => console.log(value) ).catch( error => console.log(error));
+breakfastPromise
+    .then(value => console.log(value))
+    .catch( error => console.log(error));
