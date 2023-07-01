f57f1cbeab015993cb80e0c897a146dcad29cb4b
amy_pr <ampreci@gmail.com>
2023-07-01 11:19:32 -0700

    set up catch method and message

--- js/promises-breakfast.js
+++ js/promises-breakfast.js
@@ -1,9 +1,13 @@
 const breakfastPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve('Your order is ready. Come and get it!');//What is passed to the resolve method is available to be used in the "then" method.
+        //resolve('Your order is ready. Come and get it!');//What is passed to the resolve method is available to be used in the "then" method.
+        reject('Oh no! There was a problem with your order.')
     }, 3000)
 
 });
 
 console.log(breakfastPromise)
-breakfastPromise.then( value => console.log(value) ) //To get the promise value out of the promise object (consume it), use the then() method. It can be called to handle fulfilled and rejected promises.
+//To get the promise value out of the promise object (consume it), use the then() method. 
+//It can be called to handle fulfilled and rejected promises.
+//The catch method only handles rejections, or errors.
+breakfastPromise.then( value => console.log(value) ).catch( error => console.log(error));
